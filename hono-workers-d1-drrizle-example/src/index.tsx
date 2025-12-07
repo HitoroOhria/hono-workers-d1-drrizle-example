6f7ebec6570087d0f7f7a71abf45ab7b7d73fede
hiroto.ohira <hiroto100114@gmail.com>
2025-12-07 22:50:30 +0900

    Add drizzle db to index.tsx.

--- hono-workers-d1-drrizle-example/src/index.tsx
+++ hono-workers-d1-drrizle-example/src/index.tsx
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
+import { drizzle } from 'drizzle-orm/d1';
 
 type Bindings = {
   exampleDB: D1Database;
@@ -10,6 +11,8 @@ const app = new Hono<{ Bindings: Bindings }>()
 app.use(renderer)
 
 app.get('/', async (c) => {
+  const db = drizzle(c.env.exampleDB)
+
   const {results} = await c.env.exampleDB
     .prepare("SELECT * FROM Customers WHERE CompanyName = ?")
     .bind("Bs Beverages")
