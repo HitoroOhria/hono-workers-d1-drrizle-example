1487a487f17133ea4541b2eb20f414e2936ed43a
hiroto.ohira <hiroto100114@gmail.com>
2026-01-02 14:06:36 +0900

    Query users_table with drizzle

--- hono-workers-d1-drrizle-example/src/index.tsx
+++ hono-workers-d1-drrizle-example/src/index.tsx
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
 import { drizzle } from 'drizzle-orm/d1';
+import { usersTable } from "./db/schema";
 
 type Bindings = {
   exampleDB: D1Database;
@@ -13,12 +14,9 @@ app.use(renderer)
 app.get('/', async (c) => {
   const db = drizzle(c.env.exampleDB)
 
-  const {results} = await c.env.exampleDB
-    .prepare("SELECT * FROM Customers WHERE CompanyName = ?")
-    .bind("Bs Beverages")
-    .run();
+  const result = await db.select().from(usersTable).all()
 
-  return c.json(results)
+  return c.json(result)
 })
 
 export default app
